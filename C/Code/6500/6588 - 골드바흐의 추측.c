c263d2fcd481f11229699b53366ee3589a4d8a4d
Joe2357 <50836915+Joe2357@users.noreply.github.com>
2020-08-10 23:24:04 +0900

    Solved

---
+++ C/Code/6500/6588 - 골드바흐의 추측.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_INDEX 1000000
+#define True 1
+#define False 0
+
+typedef char boolean;
+
+boolean isNotPrime[MAX_INDEX + 1] = { True, True };
+
+int main() {
+
+	// prime init
+	for (int i = 2; i <= MAX_INDEX; i++)
+		if (!isNotPrime[i])
+			for (int j = i * 2; j <= MAX_INDEX; j += i)
+				isNotPrime[j] = True;
+
+	// loop testcase
+	int n;
+	for (scanf("%d", &n); n; scanf("%d", &n))
+		for (int i = 3; i <= MAX_INDEX; i++)
+			if (!isNotPrime[i] && !isNotPrime[n - i]) {
+				printf("%d = %d + %d\n", n, i, n - i);
+				break;
+			}
+
+	return 0;
+}
